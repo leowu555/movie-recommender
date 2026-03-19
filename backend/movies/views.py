1337f9b3cf5d86f0fcddc1348eeec7ae4c8a6c9a
Leo Wu <leo05wu@gmail.com>
2026-03-18 20:43:31 -0700

    added for settings and views

--- backend/movies/views.py
+++ backend/movies/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+import os
+import requests
+from dotenv import load_dotenv
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+load_dotenv()
+
+TMDB_API_KEY = os.getenv("TMDB_API_KEY")
+
+
+@api_view(['GET'])
+def search_movies(request):
+    query = request.GET.get('query', '')
+
+    if not query:
+        return Response({"error": "Query parameter is required"}, status=400)
+
+    url = "https://api.themoviedb.org/3/search/movie"
+    params = {
+        "api_key": TMDB_API_KEY,
+        "query": query
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    return Response(data)
