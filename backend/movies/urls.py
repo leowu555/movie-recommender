42aafbdb7060329201fd12620d190079f92f7e16
Leo Wu <leo05wu@gmail.com>
2026-04-10 02:57:48 -0700

    update

--- backend/movies/urls.py
+++ backend/movies/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import search_movies, get_movie_details
 
 urlpatterns = [
+    path('search', search_movies),
     path('search/', search_movies),
     path('<int:movie_id>/', get_movie_details),
 ]
